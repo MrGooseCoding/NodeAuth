aea60e5d83b8e1d4ff389455f646e7a4afb7a86d
MrGoose Coding <mrgoosepowerful@gmail.com>
2024-03-21 19:39:15 +0100

    User model updated
    now retrieves User objects instead of raw data

--- models/user.js
+++ models/user.js
@@ -7,45 +7,56 @@ class User extends Model{
     static validator = userValidator
     static table = "users"
 
-    constructor (data = {}) {
+    constructor (data = {}) { // Note that this method does not insert anything in the database
+        "initializes a User class, without inserting anything in the database"
         super(data)
     }
 
-    static async objects_getBy (attrName, attrValue, removePassword=true) {
+    static async objects_getBy (attrName, attrValue) {
         const data = await this.__objects_getBy(attrName, attrValue);
-        if (removePassword && data[0]) {
-            delete data[1]["password"]; // It is write-only
-        }
-        return data;
+
+        return data[0] ? new User(data[1]) : data[1]
     }
 
-    static async objects_searchBy (attrName, attrValue, limit, removePassword=true) {
+    static async objects_searchBy (attrName, attrValue, limit) {
         const data = await this.__objects_searchBy(attrName, attrValue, limit)
         
+        let users = []
+        data.map(d => {users.push(new User(d))})
+
+        return users
+    }
+
+    static async authenticate (username, password) {
+        const user = await this.objects_getBy("username", username)
+        if (user["error"]) {
+            return [false, user]
+        }
+        const match = await bcrypt.compare(password, user.data.password);
+        return match ? [match, user] : [match, {}]
+    }
+
+    async change (attrName, attrValue) {
+        var data = await this.__change(User.table, attrName, attrValue)
+
+        return new User(data)
+    }
+
+    json (removePassword=true) {
+        let json = this.data
+        if (removePassword) {
+            delete json["password"]
+        }
+        return json
+    }
+    /*if (removePassword && data[0]) {
+            delete data[1]["password"]; // It is write-only
+        }
         if (removePassword) {
             data.map((v) => {
                 delete v["password"]
             })
-        }
-
-        return data
-    }
-
-    static async authenticate (username, password) {
-        const userData = await this.objects_getBy("username", username, false)
-        const match = await bcrypt.compare(password, userData[1].password);
-        delete userData[1].password
-        return [match, userData[1]]
-    }
-
-    async change (attrName, attrValue, removePassword=true) {
-        var data = await this.__change(User.table, attrName, attrValue)
-        
-        if (removePassword) {
-            delete data["password"]; // It is write-only
-        }
-        return data
-    }
+        }*/
 }
 
 module.exports = User
